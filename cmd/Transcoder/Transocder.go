f011c5d2b251e651c7b35db3658e39954c0de8d0
Devil Guru <Shubhamverma6772@gmail.com>
2026-06-15 09:31:25 +0530

    bugfix/fix-double-logging (#3)
    * bugfix/fix-double-logging
    
    * bugfix/fix-double-logging
    
    ---------
    
    Co-authored-by: Shubham Verma <shubhamv@izooto.com>

--- cmd/Transcoder/Transocder.go
+++ cmd/Transcoder/Transocder.go
@@ -1,31 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+
+	"github.com/joho/godotenv"
+
 	"video-processor/internal/config"
+	"video-processor/internal/ffmpeg"
 	"video-processor/internal/logger"
 	"video-processor/internal/rabbitmq"
-	"github.com/joho/godotenv"
 )
 
-type rmqConnection struct {
-	rabbitURL string
-	queueName string
+const TRANSCODER_VERSION = "1.0.0"
+
+type VideoJob struct {
+	VideoID  string `json:"videoId"`
+	FilePath string `json:"filePath"`
+	UserID   string `json:"userId"`
 }
 
-type rmqEnvObject struct{
-	rabbitURL string
-	queueName string
-}
 func main() {
-
 	godotenv.Load()
 
 	rmqConfig := config.LoadRMQConfigObject()
 
-	consumer, err := rabbitmq.NewConsumer(rmqConfig.RmqURL, rmqConfig.RmqQueue)
+	consumer, err := rabbitmq.NewConsumer(
+		rmqConfig.RmqURL,
+		rmqConfig.RmqQueue,
+	)
 	if err != nil {
-		logger.Error("Error Creating Consumer.. %v", err)
+		log.Fatal(err)
 	}
 	defer consumer.Close()
 
@@ -37,13 +42,58 @@ func main() {
 	logger.Info("Waiting for messages...")
 
 	for msg := range msgs {
-		logger.Info("Received: %v", string(msg.Body))
+		var job VideoJob
+
+		err := json.Unmarshal(msg.Body, &job)
+		if err != nil {
+			logger.Error("Failed to parse message: %v", err)
+
+			// Invalid message, discard it
+			msg.Nack(false, false)
+
+			continue
+		}
+
+		logger.Info("Received Video Job")
+		logger.Info("VideoID: %s", job.VideoID)
+		logger.Info("FilePath: %s", job.FilePath)
+		logger.Info("UserID: %s", job.UserID)
 
 
-		// your processing logic here
-		// e.g. FFmpeg / HLS pipeline
+		logger.Info("checking resolution of video %v", job.VideoID)
+		currentResolution, reserr := ffmpeg.CheckResolution(job.FilePath)
 
-		msg.Ack(false) //for single message acknowledgement
+		if reserr != nil{
+			logger.Error("unsupported resolution of video file: %s", job.VideoID)
+			msg.Ack(false)
+			continue
+		}
+
+		transcode_command, output_path, cmderr := ffmpeg.CommandBuilder(currentResolution, job.FilePath, job.UserID)
+
+		if cmderr != nil{
+			logger.Error("Failed to generate transcode command for video: %v, userid: %v, path: %v err: %v", job.VideoID, job.UserID, job.FilePath, err)
+			msg.Ack(false)
+			continue 
+		}
+
+		logger.Info("transcode command: %v", transcode_command);
+		logger.Info("executing command:")
+		execerr := ffmpeg.ExecuteCommand(transcode_command)
+		
+		if execerr != nil{
+			logger.Error("failed to execute: %v", transcode_command)
+			msg.Ack(false)
+			continue
+		}
+		logger.Info("video file generated at location: %s", output_path)
+
+		err = msg.Ack(false)
+		if err != nil {
+			logger.Error("ack failed: %v", err)
+			return
+		}
+
+		logger.Info("Waiting for messages...")
 	}
 }
-
